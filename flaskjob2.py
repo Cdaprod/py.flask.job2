cac90207a6b87945605e02ff1420d900688008e9
Cdaprod <davidacannan@gmail.com>
2022-11-12 14:27:13 -0500

    base.html, flaskjob2.py, worked

--- flaskjob2.py
+++ flaskjob2.py
@@ -1,11 +1,22 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask("__name__")
 
-@app.route("/<name>")
-
+@app.route("/name/")
 def home(name):
-	return render_template(index.html, content=name)
+    return render_template(index.html)
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = reques.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
-	app.run(debug=true)
+    app.run(debug=True)
